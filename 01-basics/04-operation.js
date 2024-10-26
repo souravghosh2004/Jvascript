b04262eb33d4f26320dc60d720c161fdf219849b
souravghosh2004 <souravghoshsonakuri@gmail.com>
2024-10-26 23:55:47 +0530

    operation

---
+++ 01-basics/04-operation.js
@@ -0,0 +1,76 @@
+// part1 
+console.log("Part1........................")
+console.log(5 + 2); // ans =  7
+console.log(5 - 2);  // ans =  3
+console.log(5 * 2);   // ans =  10
+console.log(5 / 2);    // ans =  2.5
+console.log(5 % 2);     // ans =  1
+console.log(5 ** 2);   //  ans =  25
+
+
+console.log();
+// part2
+console.log("Part2........................")
+let str1  = "Hello "
+let str2  = "Sourav"
+let str3 = str1+str2;
+console.log(str3);
+
+console.log();
+// part3
+console.log("Part3........................")
+console.log(1 + "2");  // ans = 12
+console.log("3" + 2);  //  ans = 32
+console.log(1 + 5+"2");  // ans = 62
+console.log("2"+3+5+6); // ans = 2356
+console.log(1 + (9+"2")); // ans  = 192
+console.log("2"+8+(5+7)); // ans = 2812
+
+console.log();
+// part4
+console.log("Part4........................")
+console.log(true+1);  // ans = 2
+console.log(false-3);  // ans = -3
+console.log(5+true);  // ans = 6
+console.log("5"+false);  // ans = 5false
+console.log(false +  "5");  // ans = false5
+
+console.log();
+// part5
+console.log("Part5........................")
+let a ;
+let b = 5;
+console.log(a+b);  // ans = NaN(not a number)
+
+console.log();
+// part6
+console.log("Part6........................")
+console.log(null+2); //  ans = 2
+console.log(null-2); //  ans = -2
+console.log(null*2); //  ans = 0
+console.log(2+null); //  ans = 2 
+console.log(null+"25");  // ans = null25 
+
+console.log();
+// part7
+console.log("Part7........................")
+console.log(undefined+null)  // ans = NaN
+console.log(undefined+2)  // ans = NaN
+console.log(undefined+2+(2+6+3))  // ans = NaN
+
+console.log();
+// part8
+console.log("Part8........................")
+console.log(null+true);   // ans = 1
+console.log(null+false);   //  ans = 0
+console.log(null+null);   // ans = 
+console.log(null+[]);  // ans = null
+
+
+
+
+
+
+
+
+
